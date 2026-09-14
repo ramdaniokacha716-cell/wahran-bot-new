becf53cd1c4ea55a5beed6d287ae141590e13478
ramdaniokacha716-cell <ramdaniokacha716@gmail.com>
2026-09-14 23:03:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,3 +159,7 @@ function generateElitePitch(businessName, activity, websiteUrl, qrUrl) {
 module.exports = {
     searchAlgerianLeads: runNational69Search
 };
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(' web server is running on port ${PORT}');
+});
